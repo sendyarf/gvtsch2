cccac039be710628c6d71d8e805d9c5bd745906e
Juy <75522716+sendyarf@users.noreply.github.com>
2026-07-15 02:23:16 +0700

    Update soco.py

--- soco.py
+++ soco.py
@@ -37,7 +37,7 @@ def setup_driver():
 def scrape_with_selenium():
     """Scrape all matches using Selenium"""
     
-    base_url = "https://socolive25.cv/"
+    base_url = "https://socolivef.cv/"
     driver = None
     
     try:
